3dfa834416d8c54a852442f5c0633165e03e629e
sundarr786 <sundarr365@gmail.com>
2024-01-16 08:47:29 +0530

    update stack

--- stack_linked_list.c
+++ stack_linked_list.c
@@ -4,3 +4,24 @@ The new node is added at begging of node, while pop it will delete begining of n
 */
 
 #include <stdio.h>
+
+#define MAX_NAME_SZ 31
+
+typedef struct student_record {
+	int id;
+	char name[MAX_NAME_SZ+1];
+	student_record *next;
+}st;
+
+void add_begin()
+{
+	st *ptr = NULL;
+	ptr = (st *)
+	return;	
+}
+
+
+int main()
+{
+	return 0;
+}
